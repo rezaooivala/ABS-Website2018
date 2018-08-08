c33809c7733fc89ab90f98270ac9ce4be007301b
Reza Alavi <rezaooivala@gmail.com>
2018-08-07 18:01:46 -0700

    Ready to upload

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+window.sr=ScrollReveal();
+
+
+
+sr.reveal('.container', {
+//    delay: 100,
+    duration: 1500,
+    origin: 'bottom',
+    distance: '50px',
+    easing: 'cubic-bezier(0, 0, 0, 1)' ,
+    interval: 100
+});
+
+
+
+
